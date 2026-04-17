b2aa3f78517ddc456692391c8a0f84892ddae1f2
Morj <111486356+ChukwudumebiOrji@users.noreply.github.com>
2026-04-17 21:59:56 +0100

    Set application to run on port 8000
    Change the application port from environment variable to a fixed port 8000.

--- main.py
+++ main.py
@@ -202,5 +202,4 @@ def delete_profile(profile_id):
     return "", 204
     
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=False, host="0.0.0.0", port=port)
+    app.run(debug=False, host="0.0.0.0", port=8000)
